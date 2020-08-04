c7d8cc838c70efd0b693a56d4de244e6cb2346a0
Sam Nolan <samnolan555@gmail.com>
2020-08-04 11:16:23 +1000

    Add workbox strategies

--- static/sw.js
+++ static/sw.js
@@ -1,45 +1,8 @@
-var cacheName = 'mayfront-page';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/main.js',
-  '/css/index.css', 
-  '/manifest.json',
-  'https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css',
-  'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+let registerRoute = workbox.routing.registerRoute;
+let StaleWhileRevalidate = workbox.strategies.StaleWhileRevalidate;
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    }),
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  if(event.request.method == 'GET'){
-    event.respondWith(
-      caches.match(event.request, {ignoreSearch: true}).then(response => {
-        setTimeout( () => {
-          cache.open(cacheName).then(cache => {
-            return fetch(event.request).then(response => {
-              cache.put(event.request, response.clone());
-              return response
-            })
-          });
-        }, 1000);
-        if(response){
-          return response;
-        }
-        else {
-          return fetch(event.request);
-        }
-      }),
-    );
-  };
-});
+registerRoute(
+  ({url}) => url.pathname.startsWith('/'),
+  new StaleWhileRevalidate()
+);
