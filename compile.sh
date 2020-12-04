2204bd1a9600a0d4b00bafad99f1ccbd85b7af29
Sam Nolan <samnolan555@gmail.com>
2020-12-04 18:17:31 +1100

    Add creating build file to compile script

--- compile.sh
+++ compile.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
+mkdir build
 if [[ -v STAGING ]]; then 
   cp src/May/Urls.elm src/May/ProdUrls.elm
   cp src/May/StageUrls.elm src/May/Urls.elm
   elm make src/TodoList.elm --output=build/main.js
   cp src/May/Urls.elm src/May/StageUrls.elm
   cp src/May/ProdUrls.elm src/May/Urls.elm
+  cp -r static/* build
+else 
+  elm make src/TodoList.elm --output=build/main.js
   cp -r static/* build
 fi;
