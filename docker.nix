96bc84b371d6f6e3007f4a1a9cd611f3c7d1c305
Sam Nolan <samnolan555@gmail.com>
2020-12-29 16:20:57 +1100

    Fix docker envirnoment

--- docker.nix
+++ docker.nix
@@ -4,14 +4,18 @@ let
 in
 # nixpkgs package set                   
 pkgs.dockerTools.buildImage { 
-    # helper to build Docker image          
     fromImage = ./alpine.tar.gz;
+    # helper to build Docker image          
     name = "elm-may";             
     # give docker image a name              
     tag = "latest";                    
     # provide a tag                         
     contents = (with pkgs; [  
-      nodePackages.yarn 
+      busybox
+      nodejs
+      bash
+      cacert
+      yarn 
       elmPackages.elm
       elmPackages.elm-format
       elm2nix
